383c345eab2dda086d9744b05bc6c2af19e68f16
“Audrey”ogambo” <Audrey.ogambo@strathmore.edu>
2022-08-31 18:49:33 +0000

    betty checks

--- shell.h
+++ shell.h
@@ -1,4 +1,20 @@
-#ifndef MAIN_H
-#define MAIN_H
-int _putchar(char c);
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <limits.h>
+#include <sys/wait.h>
+#include <signal.h>
+
+#define EXEC 1
+#define MAXARGS 128
+#define DELIM " \t\n\r\a"
+
+extern char **environ;
 #endif
