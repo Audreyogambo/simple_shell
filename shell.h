f65179d50b91783bb9d9a7a414079062fc5f5baf
“Audrey”ogambo” <Audrey.ogambo@strathmore.edu>
2022-09-01 22:00:32 +0000

    betty check corrections

--- shell.h
+++ shell.h
@@ -1,20 +1,24 @@
-#ifndef SHELL_H
-#define SHELL_H
-
+#ifndef SIMPLE_SHELL
+#define SIMPLE_SHELL
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <fcntl.h>
-#include <limits.h>
 #include <sys/wait.h>
+#include <limits.h>
 #include <signal.h>
-
-#define EXEC 1
-#define MAXARGS 128
-#define DELIM " \t\n\r\a"
-
+char *read_line(void);
+char **splits(char *line, char *delim);
+int execute(char **parse);
+void _puts(char *str);
+int _putchar(char c);
+unsigned int _strlen(char *s);
+char **find_path(char **environ);
+char *_getenv(char **environ, char *dirname);
+char *args_path(char **parse, char **tokens);
+char *_strdup(char *str);
+char *if_exists(char **environ);
 extern char **environ;
 #endif
